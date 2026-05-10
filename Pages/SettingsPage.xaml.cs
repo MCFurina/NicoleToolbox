5e3fdc18c71909f3d41ced30346ad0b8290d72ae
MCFurina <e787417731@126.com>
2026-05-10 11:06:47 +0800

    设置页已实装，新增了交互声音开关（暂无法做到重启后保持设置）

--- Pages/SettingsPage.xaml.cs
+++ Pages/SettingsPage.xaml.cs
@@ -2,6 +2,7 @@ using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
 using System;
 using Windows.Storage;
 
@@ -12,6 +13,28 @@ namespace NicoleToolbox
         public SettingsPage()
         {
             InitializeComponent();
+
+            // 如果用户已开启元素声音，则将开关设置为开启状态
+            if (ElementSoundPlayer.State == ElementSoundPlayerState.On)
+            {
+                SoundToggle.IsOn = true;
+            }
+            else
+            {
+                SoundToggle.IsOn = false;
+            }
+        }
+
+        private void SoundToggle_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (SoundToggle.IsOn)
+            {
+                ElementSoundPlayer.State = ElementSoundPlayerState.On;
+            }
+            else
+            {
+                ElementSoundPlayer.State = ElementSoundPlayerState.Off;
+            }
         }
     }
 }
